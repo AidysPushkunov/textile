f888050a61c085bac5fb0535c48124dcf789f6fb
AidysPushkunov <mywayinprogramming@gmail.com>
2022-11-21 02:48:25 +0700

    database updated

--- database.sql
+++ database.sql
@@ -42,6 +42,35 @@ INSERT INTO `admins` VALUES (1,'admin','zaq@12','root'),(8,'aidys','123','posts'
 UNLOCK TABLES;
 
 --
+-- Table structure for table `basket_users`
+--
+
+DROP TABLE IF EXISTS `basket_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `basket_users` (
+  `id_basket` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `product_id` int DEFAULT NULL,
+  PRIMARY KEY (`id_basket`),
+  KEY `user_id` (`user_id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `basket_users_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `basket_users_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `basket_users`
+--
+
+LOCK TABLES `basket_users` WRITE;
+/*!40000 ALTER TABLE `basket_users` DISABLE KEYS */;
+INSERT INTO `basket_users` VALUES (2,9,10),(6,9,12),(7,9,20),(8,9,19),(9,9,21),(10,5,8),(12,5,10),(14,2,20);
+/*!40000 ALTER TABLE `basket_users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `products`
 --
 
@@ -54,7 +83,7 @@ CREATE TABLE `products` (
   `title` varchar(255) DEFAULT NULL,
   `price` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,7 +92,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (6,'uploads/1664963159silk-1.jpg','Шёлк китайский','2394'),(7,'uploads/1664963911tkan-shelk-tkac-6.jpg','Шёлк турецкий ','25000'),(8,'uploads/1664964035SHyolk.jpg','Высококачественный шёлк','12000'),(9,'uploads/1664964171210491315-tkan-1600x1067.jpg','Хлопковый ткань','10000'),(10,'uploads/1664964279altai-republic-flag-wrinkled-on-dark-background-3d-render_1379-656.webp','Алтайский шёлк','70000'),(12,'uploads/1664964497c36ab17de6b70de1ad3798564a70ba26.jpg','Платье(готовые изделия)','7000');
+INSERT INTO `products` VALUES (6,'uploads/1664963159silk-1.jpg','Шёлк китайский','2394'),(7,'uploads/1664963911tkan-shelk-tkac-6.jpg','Шёлк турецкий ','25000'),(8,'uploads/1664964035SHyolk.jpg','Высококачественный шёлк','12000'),(9,'uploads/1664964171210491315-tkan-1600x1067.jpg','Хлопковый ткань','10000'),(10,'uploads/1664964279altai-republic-flag-wrinkled-on-dark-background-3d-render_1379-656.webp','Алтайский шёлк','70000'),(12,'uploads/1664964497c36ab17de6b70de1ad3798564a70ba26.jpg','Платье(готовые изделия)','7000'),(19,'uploads/1668970999download.jpeg','Золотой шолк','3000'),(20,'uploads/1668971057tkan-shelk-tkac-5.jpg','Шолк нвтуральный','5000'),(21,'uploads/1668971082download (2).jpeg','Шолк индонезия','4000');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,8 +108,9 @@ CREATE TABLE `users` (
   `mail` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `basket` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,7 +119,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (2,'user1@mail.com','user1','123'),(3,'user2@gmail.com','user2','1234'),(4,'user2@gmail.com','user2','1234'),(5,'mister123@gmail.com','mister-smith','mister123');
+INSERT INTO `users` VALUES (2,'user1@mail.com','user1','123',NULL),(3,'user2@gmail.com','user2','1234',5),(4,'user2@gmail.com','user2','1234',NULL),(5,'mister123@gmail.com','mister-smith','mister123',NULL),(8,'bayanashka@gmail.com','Bayana','123',NULL),(9,'aidysok@gmail.com','Aidysok','aidysok123',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -102,4 +132,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-02 15:38:22
+-- Dump completed on 2022-11-21  2:47:44
